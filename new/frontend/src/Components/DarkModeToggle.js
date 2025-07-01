39424949e7b44532d1c8ec772c9c7eb60e7cb176
Alexoswin <oswinalex1@gmail.com>
2025-07-01 07:18:38 +0530

    dark mode button

--- new/frontend/src/Components/DarkModeToggle.js
+++ new/frontend/src/Components/DarkModeToggle.js
@@ -1,22 +1,10 @@
 // DarkModeToggle.js
-import { useState, useEffect } from "react";
+
 import "./DarkModeToggle.css";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 export default function DarkModeToggle() {
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem("theme") === "dark";
-  });
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [darkMode]);
+ 
 
   return (
     <div className="toggle-wrapper">
@@ -24,8 +12,9 @@ export default function DarkModeToggle() {
         type="checkbox"
         id="toggle"
         className="toggle-checkbox"
-        checked={darkMode}
-        onChange={() => setDarkMode(!darkMode)}
+       
+       
+      
       />
       <label htmlFor="toggle" className="toggle-label">
         <span className="icon sun"><FaSun /></span>
