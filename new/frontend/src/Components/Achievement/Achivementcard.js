af35479d0a052fd1df65e3ab3b92d03dc99170fe
Alexoswin <oswinalex1@gmail.com>
2025-07-23 00:16:51 +0530

    achievement section  updated

--- new/frontend/src/Components/Achievement/Achivementcard.js
+++ new/frontend/src/Components/Achievement/Achivementcard.js
@@ -10,10 +10,10 @@ export default function Achievementcard({ name, Description, image, Date }) {
       ([entry]) => {
         if (entry.isIntersecting) {
           setIsVisible(true);
-          observer.disconnect(); // Trigger only once
+          observer.disconnect();
         }
       },
-      { threshold: 0.2 }
+      { threshold: 0.1 }
     );
 
     if (cardRef.current) {
@@ -24,17 +24,27 @@ export default function Achievementcard({ name, Description, image, Date }) {
   }, []);
 
   return (
+    <> 
+    <div className='Empty'></div>
     <div className="achievement-card-container" ref={cardRef}>
       <div className={`achievement-card ${isVisible ? 'active' : ''}`}>
         <div className="achievement-image-container">
           <img src={image} alt={name} className="achievement-image" />
+          <div className="image-overlay"></div>
         </div>
         <div className="achievement-details">
           <h3 className="achievement-name">{name}</h3>
           <p className="achievement-description">{Description}</p>
-          <p className="achievement-date">{Date}</p>
+          <div className="achievement-date-container">
+            <svg className="date-icon" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />
+            </svg>
+            <p className="achievement-date">{Date}</p>
+          </div>
         </div>
       </div>
     </div>
+     <div className='Empty'></div>
+    </>
   );
 }
--- new/frontend/src/Components/Education/EducationCard.js
+++ new/frontend/src/Components/Education/EducationCard.js
@@ -12,7 +12,9 @@ export default function EducationCard({ education }) {
       <h3 className='institute'>{education.institution}</h3>
       <p> {education.degree}</p>
       <p>{education.marks}</p>
-      <p>{formatDate(education.startDate)} - {formatDate(education.endDate)}</p>
+      <p> <svg className="date-icon" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />
+            </svg> {formatDate(education.startDate)} - {formatDate(education.endDate)}</p>
       <p> {education.issueDate}</p>
  
       
